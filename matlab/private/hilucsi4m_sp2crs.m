73adc190d1f7a17a636079ec6c87c4f8a3a9cac9
Qiao Chen <qiao.chen@stonybrook.edu>
2019-12-10 13:21:22 -0500

    Explicitly convert to int32

--- matlab/private/hilucsi4m_sp2crs.m
+++ matlab/private/hilucsi4m_sp2crs.m
@@ -5,5 +5,7 @@ assert(issparse(A), 'input A must be sparse');
 [m, n] = size(A);
 if m ~= n; error('input matrix must be squared!'); end
 [rs, cs, vs] = find(A);
-[rowptr, colind, vals] = hilucsi4m_ijv2crs(int32(m), int32(rs), int32(cs), vs);
+rs = int32(rs);
+cs = int32(cs);
+[rowptr, colind, vals] = hilucsi4m_ijv2crs(int32(m), rs, cs, vs);
 end
