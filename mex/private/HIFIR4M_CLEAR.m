0ed943823b7db6a9167f5137ae96eb17a6b8dc82
Qiao Chen <qiao.chen@stonybrook.edu>
2021-06-18 12:00:06 -0400

    Removed unused codes

--- mex/private/HIFIR4M_CLEAR.m
+++
@@ -1,5 +0,0 @@
-function flag = HIFIR4M_CLEAR
-%HIFIR4M_CLEAR - Clear the database (not deallocate)
-
-flag = int32(2);
-end
--- mex/private/HIFIR4M_QUERY.m
+++
@@ -1,6 +0,0 @@
-function flag = HIFIR4M_QUERY
-%HIFIR4M_QUERY Query stats
-
-flag = int32(HIFIR4M_M_MULTIPLY + 1);
-end
-
