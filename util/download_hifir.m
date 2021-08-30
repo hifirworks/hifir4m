cb1d9f9a264cc6ab0928dafda275bd97ed266550
Qiao Chen <qiao.chen@stonybrook.edu>
2021-08-30 17:58:25 -0400

    Added download hifir script

---
+++ util/download_hifir.m
@@ -0,0 +1,12 @@
+function path_to_hifir = download_hifir(version)
+% Download hifir
+
+zip_file = fullfile(tempdir, 'hifir.zip');
+zip_url = ['https://github.com/hifirworks/hifir/archive/refs/tags/v' ...
+    version ...
+    '.zip'];
+websave(zip_file, zip_url);
+unzip(zip_file, tempdir);
+delete(zip_file);
+path_to_hifir = fullfile(tempdir, ['hifir-' version]);
+end
