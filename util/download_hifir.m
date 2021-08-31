278fe219f4c918611884f4daf141ba443d0a98ad
Qiao Chen <qiao.chen@stonybrook.edu>
2021-08-31 19:19:08 -0400

    Changed to urlwrite

--- util/download_hifir.m
+++ util/download_hifir.m
@@ -5,7 +5,7 @@ zip_file = fullfile(tempdir, 'hifir.zip');
 zip_url = ['https://github.com/hifirworks/hifir/archive/refs/tags/v' ...
     version ...
     '.zip'];
-websave(zip_file, zip_url);
+urlwrite(zip_url, zip_file);
 unzip(zip_file, tempdir);
 delete(zip_file);
 path_to_hifir = fullfile(tempdir, ['hifir-' version]);
